f782fa824cd39bef450f4c439e18e4c127acfec0
Rishabh Singh <rishimark98@gmail.com>
2021-08-05 22:34:19 +0530

    updating login screen ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter2/screens/Home.dart';
+import 'package:flutter2/screens/Login.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Learning App",
-      routes: {
-        "/": (context) =>  HomePage(),
-      },
+      title: "Login UI",
+      home: LoginScreen(),
+     debugShowCheckedModeBanner: false,
     );
   }
 }
--- lib/screens/Login.dart
+++ lib/screens/Login.dart
@@ -1,12 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class Login extends StatelessWidget {
-  const Login({ Key? key }) : super(key: key);
-
+class LoginScreen extends StatefulWidget {
   @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          child: Stack(
+            children: <Widget>[
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color(0x665ac18e),
+                      Color(0x995ac18e),
+                      Color(0xcc5ac18e),
+                      Color(0xff5ac18e),
+                      ]
+                  )
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Sign In",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 50),
+                    
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
